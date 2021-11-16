f9e53dae7f09934943802e702629542d7b650262
sirazulsihab <smsisihab@gmail.com>
2021-11-17 00:37:43 +0600

    env added

--- src/Firebase/Firebase.config.js
+++ src/Firebase/Firebase.config.js
@@ -1,11 +1,10 @@
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyCkn0o_1eielRPUPF4ZBRrpOkb0l22G2nM",
-    authDomain: "travel-agency-f07f8.firebaseapp.com",
-    projectId: "travel-agency-f07f8",
-    storageBucket: "travel-agency-f07f8.appspot.com",
-    messagingSenderId: "786172202098",
-    appId: "1:786172202098:web:ce9c571e7e4aa4166046eb"
-  };
-
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+};
   export default firebaseConfig;
